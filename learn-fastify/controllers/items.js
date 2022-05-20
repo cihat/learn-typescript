426e3b7595777f467b16f0877017b049b08f5eec
cihat <cihat02020202@gmail.com>
2022-05-20 09:29:20 +0300

    Add items.s controller

---
+++ learn-fastify/controllers/items.js
@@ -0,0 +1,32 @@
+let items = require("../Items");
+const { v4: uuidv4 } = require("uuid");
+
+const getItems = (req, reply) => {
+  reply.send(items);
+};
+
+const getItem = (req, reply) => {
+  const { id } = req.params;
+
+  const item = items.find((item) => (item.id = id));
+  reply.send(item);
+};
+
+const addItem = (req, reply) => {
+  const { name } = req.body;
+  const item = {
+    id: uuidv4(),
+    name,
+  };
+
+  items = [...items, item];
+  
+
+  reply.code(201).send(items);
+};
+
+module.exports = {
+  getItems,
+  getItem,
+  addItem,
+};
